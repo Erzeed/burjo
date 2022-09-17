1072fd362d329fda28f8faa40a8aa789b90da5f1
feizal reza <feizalreza@feizals-MacBook-Pro.local>
2022-09-17 21:31:33 +0700

    add service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import './views/components/footer/footer';
 import './views/components/text-slider/text-slider';
 import './views/components/loading/loading';
 import './data/data-restaurant';
+import swRegister from './utils/sw-register';
 import App from './views/app';
 
 
@@ -12,7 +13,6 @@ const app = new App({
   drawer: document.querySelector('nav-bar').shadowRoot.querySelector('nav .nav__list'),
   iconBar: document.querySelector('nav-bar').shadowRoot.querySelector('nav button img'),
   button: document.querySelector('nav-bar').shadowRoot.querySelector('#hamburgerButton'),
-  loading: document.querySelector('load-ding').shadowRoot.querySelector('.page__loading'),
   content: document.querySelector('#mainContent'),
 });
 
@@ -23,7 +23,7 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', async () => {
   app.renderPage();
-  // await swRegister();
+  swRegister();
   // WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 
   // FooterToolsInitiator.init({
